97201040420c30059d503c8ae8d6680a50ff5308
Austin Green <austinzgreen@gmail.com>
2024-01-30 13:50:37 -0500

    chore: rm env id

--- app/RootApp.tsx
+++ app/RootApp.tsx
@@ -6,7 +6,7 @@ import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core'
 const RootApp = ({ children }: { children: React.ReactNode }) => (
   <DynamicContextProvider
     settings={{
-      environmentId: '42c44b80-2240-40ea-8e01-23b7e88e26a6',
+      environmentId: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX',
       walletConnectors: [EthereumWalletConnectors],
     }}
   >
